366e0a60eacf181aa9dacc330099be7b8d39d6a8
David Shorthouse <davidpshorthouse@gmail.com>
2018-08-06 13:19:10 -0400

    Get/set search param

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,43 @@
+var updateQueryStringParam = function (key, value) {
+
+    var baseUrl = [location.protocol, '//', location.host, location.pathname].join(''),
+        urlQueryString = document.location.search,
+        newParam = key + '=' + value,
+        params = '?' + newParam;
+
+    // If the "search" string exists, then build params from it
+    if (urlQueryString) {
+        var updateRegex = new RegExp('([\?&])' + key + '[^&]*');
+        var removeRegex = new RegExp('([\?&])' + key + '=[^&;]+[&;]?');
+
+        if( typeof value == 'undefined' || value == null || value == '' ) { // Remove param if value is empty
+            params = urlQueryString.replace(removeRegex, "$1");
+            params = params.replace( /[&;]$/, "" );
+
+        } else if (urlQueryString.match(updateRegex) !== null) { // If param exists already, update it
+            params = urlQueryString.replace(updateRegex, "$1" + newParam);
+
+        } else { // Otherwise, add it to end of query string
+            params = urlQueryString + '&' + newParam;
+        }
+    }
+
+    // no parameter was set so we don't need the question mark
+    params = params == '?' ? '' : params;
+
+    window.history.replaceState({}, "", baseUrl + params);
+};
+
+var getParameterByName = function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 $(function() {
 
   var bookmarks = {};
@@ -47,6 +87,9 @@ $(function() {
         }
         if (search) { 
           $.extend(d.selector, or_clause);
+          updateQueryStringParam('q', d.search["value"]);
+        } else {
+          updateQueryStringParam('q', '');
         }
         delete(d["columns"]);
         delete(d["draw"]);
@@ -123,6 +166,7 @@ $(function() {
         onRegionClick: function(e, code) {
           if (countryData.hasOwnProperty(code)) {
             $('#taxonomists_filter').find('input').val(countryData[code].name);
+            updateQueryStringParam('q', countryData[code].name);
             table.search(countryData[code].name).draw();
           }
           $('#map-modal').modal('toggle');
@@ -130,4 +174,9 @@ $(function() {
       });
     }
   });
+
+  if (getParameterByName('q')) {
+    table.search(getParameterByName('q')).draw();
+  }
+
 });
